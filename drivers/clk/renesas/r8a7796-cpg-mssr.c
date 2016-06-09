26c86e2df3a498c0a4ecdb566f96311ccefa0831
Iurii Konovalenko <iurii.konovalenko@globallogic.com>
2016-06-09 14:20:58 +0300

    clk:shmobile: Hide clock for SCIF2
    On Renesas Salvaotor board with Xen SCIF2 is provided to hypervisor, so
    kernel should not manage SCIF2 clock.
    
    Signed-off-by: Iurii Konovalenko <iurii.konovalenko@globallogic.com>
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- drivers/clk/renesas/r8a7796-cpg-mssr.c
+++ drivers/clk/renesas/r8a7796-cpg-mssr.c
@@ -157,7 +157,7 @@ static struct mssr_mod_clk r8a7796_mod_clks[] __initdata = {
 	DEF_MOD("cmt1",			 302,	R8A7796_CLK_R),
 	DEF_MOD("cmt0",			 303,	R8A7796_CLK_R),
 	DEF_MOD("tpu0",			 304,	R8A7796_CLK_S3D4),
-	DEF_MOD("scif2",		 310,	R8A7796_CLK_S3D4),
+	/*DEF_MOD("scif2",		 310,	R8A7796_CLK_S3D4),*/
 	DEF_MOD("sdif3",		 311,	R8A7796_CLK_SD3),
 	DEF_MOD("sdif2",		 312,	R8A7796_CLK_SD2),
 	DEF_MOD("sdif1",		 313,	R8A7796_CLK_SD1),
