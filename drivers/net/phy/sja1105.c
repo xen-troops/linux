21d16e3c4d6c60d53c9955f422605cbf3f1bad67
Matthias Blankertz <matthias.blankertz@cetitec.com>
2017-11-30 10:36:14 +0100

    [phy/sja1105] Make dummy PHY link speed configurable
    Add a device tree property "speed" to the SJA1105 dummy PHY to configure
    link speed (100 or 1000).
    
    (cherry picked from commit deb9ec09ad7085b207055c2d9e1f6640e5a0d824)
    (cherry picked from commit 6b672040d6d41be5cd50b9b5a45a04829f06fa9c)

--- drivers/net/phy/sja1105.c
+++ drivers/net/phy/sja1105.c
@@ -20,14 +20,26 @@
 #include <linux/mii.h>
 #include <linux/ethtool.h>
 #include <linux/phy.h>
+#include <linux/of.h>
 
 MODULE_DESCRIPTION("SJA1105 dummy PHY driver");
 MODULE_AUTHOR("Michael Wegner");
 MODULE_LICENSE("GPL");
 
+struct sja1105_phy_priv {
+	u32 speed;
+};
+
 static int sja1105_config_aneg(struct phy_device *phydev)
 {
-	phydev->supported = (PHY_DEFAULT_FEATURES | PHY_100BT_FEATURES);
+	struct sja1105_phy_priv *priv = phydev->priv;
+
+	phydev->supported = PHY_DEFAULT_FEATURES;
+	if (priv->speed == 1000) {
+		phydev->supported |= PHY_1000BT_FEATURES;
+	} else if (priv->speed == 100) {
+		phydev->supported |= PHY_100BT_FEATURES;
+	}
 	phydev->advertising = phydev->supported;
 
 	return 0;
@@ -35,20 +47,60 @@ static int sja1105_config_aneg(struct phy_device *phydev)
 
 static int sja1105_read_status(struct phy_device *phydev)
 {
+	struct sja1105_phy_priv *priv = phydev->priv;
+
 	phydev->duplex = DUPLEX_FULL;
-	phydev->speed = SPEED_100;
+	phydev->speed = priv->speed;
 	phydev->pause = 0;
 	phydev->asym_pause = 0;
 
 	return 0;
 }
 
+static int sja1105_phy_probe(struct phy_device *phydev)
+{
+	struct device *dev = &phydev->mdio.dev;
+	struct device_node *of_node = dev->of_node;
+	struct sja1105_phy_priv *priv;
+
+	priv = devm_kzalloc(dev, sizeof(struct sja1105_phy_priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	if (of_property_read_u32(of_node, "speed", &priv->speed) != 0) {
+		dev_info(dev, "Could not read speed property, using default 100");
+		priv->speed = 100;
+	}
+
+	if (priv->speed != 1000 && priv->speed != 100) {
+		dev_err(dev, "Invalid link speed %u, must be 100 or 1000", priv->speed);
+		kfree(priv);
+		return -EINVAL;
+	}
+
+	phydev->priv = priv;
+
+	return 0;
+}
+
+static void sja1105_phy_remove(struct phy_device *phydev)
+{
+	if (ZERO_OR_NULL_PTR(phydev)) {
+		return;
+	}
+
+	kfree(phydev->priv);
+}
+
 static struct phy_driver sja1105_driver[] = { {
 	.phy_id		= 0xfffffffe,
 	.name		= "SJA1105 Dummy PHY driver",
 	.phy_id_mask	= 0x0ffffff0,
-	.features	= (PHY_DEFAULT_FEATURES | PHY_100BT_FEATURES),
+	.features	= (PHY_DEFAULT_FEATURES | PHY_1000BT_FEATURES |
+				   PHY_1000BT_FEATURES),
 	.flags		= PHY_POLL,
+	.probe      = sja1105_phy_probe,
+	.remove     = sja1105_phy_remove,
 	.config_aneg	= sja1105_config_aneg,
 	.read_status	= sja1105_read_status,
 } };
