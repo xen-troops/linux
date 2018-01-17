4907e1aaf8638a9fe0c4a5af4c2bba55da3dd8ab
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2018-01-17 15:36:06 +0200

    rcar-ion: Define ion heaps configuration statically
    - Statically define ion heaps
    - Added system ion heap
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Reviewed-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- drivers/staging/android/ion/rcar/rcar_ion.c
+++ drivers/staging/android/ion/rcar/rcar_ion.c
@@ -12,9 +12,7 @@
 #include <linux/miscdevice.h>
 #include <linux/of.h>
 
-static int num_heaps;
 static struct ion_heap **g_apsIonHeaps;
-static struct ion_platform_heap **rcar_ion_heaps_data;
 
 struct ion_device *g_psIonDev;
 EXPORT_SYMBOL(g_psIonDev);
@@ -22,6 +20,21 @@ EXPORT_SYMBOL(g_psIonDev);
 #include <linux/dma-mapping.h>
 #include <linux/vmalloc.h>
 #include <linux/uaccess.h>
+
+static struct ion_platform_heap rcar_ion_heaps[] = {
+        {
+            .id = ION_HEAP_TYPE_SYSTEM,
+            .type   = ION_HEAP_TYPE_SYSTEM,
+            .name   = "rcar_system",
+        },
+        {
+            .id = ION_HEAP_TYPE_DMA,
+            .type   = ION_HEAP_TYPE_DMA,
+            .name   = "rcar_cma",
+        },
+};
+
+static int num_heaps = ARRAY_SIZE(rcar_ion_heaps);
 
 enum {
     RCAR_ION_IOC_CUSTOM_GETPHYADDR = 1,
@@ -31,55 +44,6 @@ struct rcar_ion_getphys_data {
     int fd;
     uint64_t paddr;
 };
-
-static int rcar_ion_set_platform_data(struct platform_device *pdev)
-{
-	const char *heap_name;
-	int ret;
-	struct device_node *np;
-	struct ion_platform_heap *p_data;
-	const struct device_node *dt_node = pdev->dev.of_node;
-	int index = 0;
-
-	for_each_child_of_node(dt_node, np)
-		num_heaps++;
-
-	rcar_ion_heaps_data = devm_kzalloc(&pdev->dev,
-				  sizeof(struct ion_platform_heap *) *
-				  num_heaps,
-				  GFP_KERNEL);
-	if (!rcar_ion_heaps_data)
-		return -ENOMEM;
-
-	for_each_child_of_node(dt_node, np) {
-		ret = of_property_read_string(np, "heap-name", &heap_name);
-		if (ret < 0) {
-			pr_err("check the name of node %s\n", np->name);
-			continue;
-		}
-
-		p_data = devm_kzalloc(&pdev->dev,
-				      sizeof(struct ion_platform_heap),
-				      GFP_KERNEL);
-		if (!p_data)
-			return -ENOMEM;
-
-		p_data->priv = &pdev->dev;
-		p_data->name = heap_name;
-		p_data->base = 0;
-		p_data->size = 0;
-
-		p_data->id = ION_HEAP_TYPE_DMA;
-		p_data->type = ION_HEAP_TYPE_DMA;
-
-		printk("ion: heap index %d : name %s base 0x%lx size 0x%lx id %d type %d\n",
-			index, p_data->name, p_data->base, p_data->size, p_data->id, p_data->type);
-
-		rcar_ion_heaps_data[index] = p_data;
-		index++;
-	}
-	return 0;
-}
 
 static long rcar_ion_get_phys_addr(struct ion_client *client, unsigned long arg)
 {
@@ -135,13 +99,6 @@ int rcar_ion_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, g_psIonDev);
 
-	err = rcar_ion_set_platform_data(pdev);
-	if (err) {
-		pr_err("ion set platform data error!\n");
-		ion_device_destroy(g_psIonDev);
-		return err;
-	}
-
 	g_apsIonHeaps = devm_kzalloc(&pdev->dev, sizeof(struct ion_heap *) * num_heaps, GFP_KERNEL);
 	if (!g_apsIonHeaps) {
 		ion_device_destroy(g_psIonDev);
@@ -150,15 +107,16 @@ int rcar_ion_probe(struct platform_device *pdev)
 
 	/* Register all the heaps */
 	for (i = 0; i < num_heaps; i++) {
-		struct ion_platform_heap *psPlatHeapData = rcar_ion_heaps_data[i];
-
-		g_apsIonHeaps[i] = ion_heap_create(psPlatHeapData);
+		rcar_ion_heaps[i].priv = &pdev->dev;
+		g_apsIonHeaps[i] = ion_heap_create(&rcar_ion_heaps[i]);
 
 		if (IS_ERR_OR_NULL(g_apsIonHeaps[i])) {
 			err = PTR_ERR(g_apsIonHeaps[i]);
 			goto failHeapCreate;
 		}
 		ion_device_add_heap(g_psIonDev, g_apsIonHeaps[i]);
+		dev_info(&pdev->dev, "ion heap: name %s id %d type %d\n",
+			rcar_ion_heaps[i].name, rcar_ion_heaps[i].id, rcar_ion_heaps[i].type);
 	}
 
 	dev_info(&pdev->dev, "Ion initialized!\n");
