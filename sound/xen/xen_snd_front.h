72130411a689d757ac8ba6c3760cd4d2fe18cc58
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-03-02 18:04:14 +0200

    ALSA: xen-front: Implement ALSA virtual sound driver
    Implement essential initialization of the sound driver:
      - introduce required data structures
      - handle driver registration
      - handle sound card registration
      - register sound driver on backend connection
      - remove sound driver on backend disconnect
    
    Initialize virtual sound card with streams according to the
    Xen store configuration.
    
    Implement ALSA driver operations including:
    - manage frontend/backend shared buffers
    - manage Xen bus event channel states
    
    Implement requests from front to back for ALSA
    PCM operations.
     - report ALSA period elapsed event: handle XENSND_EVT_CUR_POS
       notifications from the backend when stream position advances
       during playback/capture. The event carries a value of how
       many octets were played/captured at the time of the event.
     - implement explicit stream parameter negotiation between
       backend and frontend: handle XENSND_OP_HW_PARAM_QUERY request
       to read/update configuration space for the parameter given:
       request passes desired parameter interval and the response to
       this request returns min/max interval for the parameter to be used.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- sound/xen/xen_snd_front.h
+++ sound/xen/xen_snd_front.h
@@ -13,16 +13,44 @@
 
 #include "xen_snd_front_cfg.h"
 
+struct card_info;
+struct xen_snd_front_evtchnl;
 struct xen_snd_front_evtchnl_pair;
+struct xen_snd_front_shbuf;
+struct xensnd_query_hw_param;
 
 struct xen_snd_front_info {
 	struct xenbus_device *xb_dev;
 
+	struct card_info *card_info;
+
 	spinlock_t io_lock;
+
 	int num_evt_pairs;
 	struct xen_snd_front_evtchnl_pair *evt_pairs;
 
 	struct xen_front_cfg_card cfg;
 };
+
+int xen_snd_front_stream_query_hw_param(struct xen_snd_front_evtchnl *evtchnl,
+		struct xensnd_query_hw_param *hw_param_req,
+		struct xensnd_query_hw_param *hw_param_resp);
+
+int xen_snd_front_stream_prepare(struct xen_snd_front_evtchnl *evtchnl,
+		struct xen_snd_front_shbuf *sh_buf,
+		uint8_t format, unsigned int channels,
+		unsigned int rate, uint32_t buffer_sz,
+		uint32_t period_sz);
+
+int xen_snd_front_stream_close(struct xen_snd_front_evtchnl *evtchnl);
+
+int xen_snd_front_stream_write(struct xen_snd_front_evtchnl *evtchnl,
+		unsigned long pos, unsigned long count);
+
+int xen_snd_front_stream_read(struct xen_snd_front_evtchnl *evtchnl,
+		unsigned long pos, unsigned long count);
+
+int xen_snd_front_stream_trigger(struct xen_snd_front_evtchnl *evtchnl,
+		int type);
 
 #endif /* __XEN_SND_FRONT_H */
