22f153593f64291bf2c4432451b7e578e374c582
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-03-02 17:54:54 +0200

    ALSA: xen-front: Implement Xen event channel handling
    Handle Xen event channels:
      - create for all configured streams and publish
        corresponding ring references and event channels in Xen store,
        so backend can connect
      - implement event channels interrupt handlers
      - create and destroy event channels with respect to Xen bus state
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- sound/xen/xen_snd_front.h
+++ sound/xen/xen_snd_front.h
@@ -13,8 +13,14 @@
 
 #include "xen_snd_front_cfg.h"
 
+struct xen_snd_front_evtchnl_pair;
+
 struct xen_snd_front_info {
 	struct xenbus_device *xb_dev;
+
+	spinlock_t io_lock;
+	int num_evt_pairs;
+	struct xen_snd_front_evtchnl_pair *evt_pairs;
 
 	struct xen_front_cfg_card cfg;
 };
