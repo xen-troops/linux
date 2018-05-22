4ae8e1b21de7509b57b3bcb4e04f85ec54d37f10
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-05-22 17:13:04 +0300

    drm: Fix possible race conditions while unplugging DRM device
    [ Upstream commit 069035c5db3459b9b5f12caf3bffed9a863fa5c4 ]
    
    When unplugging a hotpluggable DRM device we first unregister it
    with drm_dev_unregister and then set drm_device.unplugged flag which
    is used to mark device critical sections with drm_dev_enter()/
    drm_dev_exit() preventing access to device resources that are not
    available after the device is gone.
    But drm_dev_unregister may lead to hotplug uevent(s) fired to
    user-space on card and/or connector removal, thus making it possible
    for user-space to try accessing a disconnected device.
    
    Fix this by first making sure device is properly marked as
    disconnected and only then unregister it.
    
    Fixes: bee330f3d672 ("drm: Use srcu to protect drm_device.unplugged")
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reported-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Cc: "Noralf Trønnes" <noralf@tronnes.org>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180522141304.18646-1-andr2000@gmail.com

--- drivers/gpu/drm/drm_drv.c
+++ drivers/gpu/drm/drm_drv.c
@@ -417,13 +417,6 @@ EXPORT_SYMBOL(drm_dev_exit);
  */
 void drm_dev_unplug(struct drm_device *dev)
 {
-	drm_dev_unregister(dev);
-
-	mutex_lock(&drm_global_mutex);
-	if (dev->open_count == 0)
-		drm_dev_unref(dev);
-	mutex_unlock(&drm_global_mutex);
-
 	/*
 	 * After synchronizing any critical read section is guaranteed to see
 	 * the new value of ->unplugged, and any critical section which might
@@ -432,6 +425,13 @@ void drm_dev_unplug(struct drm_device *dev)
 	 */
 	dev->unplugged = true;
 	synchronize_srcu(&drm_unplug_srcu);
+
+	drm_dev_unregister(dev);
+
+	mutex_lock(&drm_global_mutex);
+	if (dev->open_count == 0)
+		drm_dev_unref(dev);
+	mutex_unlock(&drm_global_mutex);
 }
 EXPORT_SYMBOL(drm_dev_unplug);
 
