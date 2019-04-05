379c12b1d0a3a55fe20b89b8ffc927cb0b188046
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2019-04-05 10:30:29 +0300

    xen-netfront: Disable resume callback
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -1431,11 +1431,13 @@ static void xennet_disconnect_backend(struct netfront_info *info)
  */
 static int netfront_resume(struct xenbus_device *dev)
 {
+#if 0
 	struct netfront_info *info = dev_get_drvdata(&dev->dev);
 
 	dev_dbg(&dev->dev, "%s\n", dev->nodename);
 
 	xennet_disconnect_backend(info);
+#endif
 	return 0;
 }
 
