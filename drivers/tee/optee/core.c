e9f86e68880ed39e05737d7850372b6e14bf17a4
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2020-10-06 21:10:38 +0300

    optee: don't fail if devices can't be enumerated
    OP-TEE devices are optional feature, not all platforms support it, so
    there is no need to fail there.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- drivers/tee/optee/core.c
+++ drivers/tee/optee/core.c
@@ -654,7 +654,7 @@ static struct optee *optee_probe(struct device_node *np)
 
 	rc = optee_enumerate_devices();
 	if (rc)
-		goto err;
+		pr_warn("Can't enumerate OP-TEE devices: %d\n", rc);
 
 	pr_info("initialized driver\n");
 	return optee;
