91760c02542821fc5c8dc856b71ebb51c51fade9
Dmytro Firsov <dmytro_firsov@epam.com>
2022-09-14 12:04:43 +0300

    drivers: net: renesas: separate flower matcher setup to a separate function
    This commit moves match parsing for R-Switch TC flower filter to
    dedicated function to make it common with action parsing.
    
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- drivers/net/ethernet/renesas/rswitch_tc_flower.c
+++ drivers/net/ethernet/renesas/rswitch_tc_flower.c
@@ -100,6 +100,256 @@ static int rswitch_tc_flower_setup_redirect_action(struct rswitch_tc_filter *f)
 	return 0;
 }
 
+static int rswitch_tc_flower_setup_match(struct rswitch_tc_filter *f,
+				struct flow_rule *rule)
+{
+	struct rswitch_pf_param pf_param = {0};
+	int rc = 0, i;
+	u16 addr_type = 0;
+
+	pf_param.rdev = f->rdev;
+	pf_param.all_sources = false;
+
+	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_CONTROL)) {
+		struct flow_match_control match;
+
+		flow_rule_match_control(rule, &match);
+		addr_type = match.key->addr_type;
+	} else if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_IPV4_ADDRS)) {
+		addr_type = FLOW_DISSECTOR_KEY_IPV4_ADDRS;
+	} else if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_IPV6_ADDRS)) {
+		addr_type = FLOW_DISSECTOR_KEY_IPV6_ADDRS;
+	}
+
+	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_BASIC)) {
+		struct flow_match_basic match;
+
+		flow_rule_match_basic(rule, &match);
+
+		if (match.mask->n_proto) {
+			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE,
+					ntohs(match.key->n_proto), ntohs(match.mask->n_proto),
+					RSWITCH_IP_VERSION_OFFSET);
+			if (rc)
+				return rc;
+		}
+
+		if (match.mask->ip_proto) {
+			/* Using one byte in two-byte filter, make offset correction */
+			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE,
+					match.key->ip_proto, match.mask->ip_proto,
+					RSWITCH_IPV4_PROTO_OFFSET - 1);
+			if (rc)
+				return rc;
+		}
+	}
+
+	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_ETH_ADDRS)) {
+		struct flow_match_eth_addrs match;
+
+		flow_rule_match_eth_addrs(rule, &match);
+
+		/*
+		 * There two possible scenarios for both src and dst MAC
+		 * matching (when mask is non-zero):
+		 * - MAC is not masked (mask is ff:...:ff) and we can use
+		 *   1 three-byte filter in expand mode
+		 * - MAC is masked and we need to use 2 three-byte filters.
+		 *   Both MAC and mask will be divided into 2 parts and will
+		 *   be placed to separate filters.
+		 */
+		if (!is_zero_ether_addr(match.mask->src)) {
+			if (is_broadcast_ether_addr(match.mask->src)) {
+				rc = rswitch_init_expand_pf_entry(&pf_param, PF_THREE_BYTE,
+						rswitch_mac_left_half(match.key->src),
+						rswitch_mac_right_half(match.key->src),
+						RSWITCH_MAC_SRC_OFFSET);
+				if (rc)
+					return rc;
+			} else {
+				rc = rswitch_init_mask_pf_entry(&pf_param, PF_THREE_BYTE,
+						rswitch_mac_left_half(match.key->src),
+						rswitch_mac_left_half(match.mask->src),
+						RSWITCH_MAC_SRC_OFFSET);
+				if (rc)
+					return rc;
+
+				rc = rswitch_init_mask_pf_entry(&pf_param, PF_THREE_BYTE,
+						rswitch_mac_right_half(match.key->src),
+						rswitch_mac_right_half(match.mask->src),
+						RSWITCH_MAC_SRC_OFFSET + 3);
+				if (rc)
+					return rc;
+			}
+		}
+
+		if (!is_zero_ether_addr(match.mask->dst)) {
+			if (is_broadcast_ether_addr(match.mask->dst)) {
+				rc = rswitch_init_expand_pf_entry(&pf_param, PF_THREE_BYTE,
+							rswitch_mac_left_half(match.key->dst),
+							rswitch_mac_right_half(match.key->dst),
+							RSWITCH_MAC_DST_OFFSET);
+				if (rc)
+					return rc;
+			} else {
+				rc = rswitch_init_mask_pf_entry(&pf_param, PF_THREE_BYTE,
+						rswitch_mac_left_half(match.key->dst),
+						rswitch_mac_left_half(match.mask->dst),
+						RSWITCH_MAC_DST_OFFSET);
+				if (rc)
+					return rc;;
+
+				rc = rswitch_init_mask_pf_entry(&pf_param, PF_THREE_BYTE,
+						rswitch_mac_right_half(match.key->dst),
+						rswitch_mac_right_half(match.mask->dst),
+						RSWITCH_MAC_DST_OFFSET + 3);
+				if (rc)
+					return rc;
+			}
+		}
+	}
+
+	if (addr_type == FLOW_DISSECTOR_KEY_IPV4_ADDRS) {
+		struct flow_match_ipv4_addrs match;
+
+		flow_rule_match_ipv4_addrs(rule, &match);
+
+		if (match.mask->src) {
+			rc = rswitch_init_mask_pf_entry(&pf_param, PF_FOUR_BYTE,
+					be32_to_cpu(match.key->src), be32_to_cpu(match.mask->src),
+					RSWITCH_IPV4_SRC_OFFSET);
+			if (rc)
+				return rc;
+		}
+
+		if (match.mask->dst) {
+			rc = rswitch_init_mask_pf_entry(&pf_param, PF_FOUR_BYTE,
+					be32_to_cpu(match.key->dst), be32_to_cpu(match.mask->dst),
+					RSWITCH_IPV4_DST_OFFSET);
+			if (rc)
+				return rc;
+		}
+	}
+
+	if (addr_type == FLOW_DISSECTOR_KEY_IPV6_ADDRS) {
+		struct flow_match_ipv6_addrs match;
+
+		flow_rule_match_ipv6_addrs(rule, &match);
+
+		/*
+		 * Approach is the same as for MAC addresses:
+		 * - 2 four-byte filters in expand mode for exact IPv6
+		 * - 4 four-byte filter in mask mode for masked IPv6 (or less
+		 *   if mask has more than 32 zeros at the end)
+		 */
+		if (!rswitch_ipv6_all_zero(&match.mask->src)) {
+			if (rswitch_ipv6_all_set(&match.mask->src)) {
+				rc = rswitch_init_expand_pf_entry(&pf_param, PF_FOUR_BYTE,
+						be32_to_cpu(match.key->src.s6_addr32[0]),
+						be32_to_cpu(match.key->src.s6_addr32[1]),
+						RSWITCH_IPV6_SRC_OFFSET);
+				if (rc)
+					return rc;
+
+				rc = rswitch_init_expand_pf_entry(&pf_param, PF_FOUR_BYTE,
+						be32_to_cpu(match.key->src.s6_addr32[2]),
+						be32_to_cpu(match.key->src.s6_addr32[3]),
+						RSWITCH_IPV6_SRC_OFFSET + 8);
+				if (rc)
+					return rc;
+			} else {
+				/* Walk through all 128-bit or until we reach zero mask in addr */
+				for (i = 0; (i < 4) && match.mask->src.s6_addr32[i]; i++) {
+					rc = rswitch_init_mask_pf_entry(&pf_param, PF_FOUR_BYTE,
+							be32_to_cpu(match.key->src.s6_addr32[i]),
+							be32_to_cpu(match.mask->src.s6_addr32[i]),
+							RSWITCH_IPV6_SRC_OFFSET + 4 * i);
+					if (rc)
+						return rc;
+				}
+			}
+		}
+
+		if (!rswitch_ipv6_all_zero((&match.mask->dst))) {
+			if (rswitch_ipv6_all_set(&match.mask->dst)) {
+				rc = rswitch_init_expand_pf_entry(&pf_param, PF_FOUR_BYTE,
+						be32_to_cpu(match.key->dst.s6_addr32[0]),
+						be32_to_cpu(match.key->dst.s6_addr32[1]),
+						RSWITCH_IPV6_DST_OFFSET);
+				if (rc)
+					return rc;
+
+				rc = rswitch_init_expand_pf_entry(&pf_param, PF_FOUR_BYTE,
+						be32_to_cpu(match.key->dst.s6_addr32[2]),
+						be32_to_cpu(match.key->dst.s6_addr32[3]),
+						RSWITCH_IPV6_DST_OFFSET + 8);
+				if (rc)
+					return rc;
+			} else {
+				/* Walk through all 128-bit or until we reach zero mask in addr */
+				for (i = 0; (i < 4) && match.mask->dst.s6_addr32[i]; i++) {
+					rc = rswitch_init_mask_pf_entry(&pf_param, PF_FOUR_BYTE,
+							be32_to_cpu(match.key->dst.s6_addr32[i]),
+							be32_to_cpu(match.mask->dst.s6_addr32[i]),
+							RSWITCH_IPV6_DST_OFFSET + 4 * i);
+					if (rc)
+						return rc;
+				}
+			}
+		}
+	}
+
+	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_IP)) {
+		struct flow_match_ip match;
+
+		flow_rule_match_ip(rule, &match);
+
+		if (match.mask->tos) {
+			/* Using one byte in two-byte filter, make offset correction */
+			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE, match.key->tos,
+					match.mask->tos, RSWITCH_IPV4_TOS_OFFSET - 1);
+			if (rc)
+				return rc;
+		}
+
+		if (match.mask->ttl) {
+			/* Using one byte in two-byte filter, make offset correction */
+			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE, match.key->ttl,
+					match.mask->ttl, RSWITCH_IPV4_TTL_OFFSET - 1);
+			if (rc)
+				return rc;
+		}
+	}
+
+	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_PORTS)) {
+		struct flow_match_ports match;
+
+		flow_rule_match_ports(rule, &match);
+
+		if (match.mask->src) {
+			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE,
+					be16_to_cpu(match.key->src), be16_to_cpu(match.mask->src),
+					RSWITCH_L4_SRC_PORT_OFFSET);
+			if (rc)
+				return rc;
+		}
+
+		if (match.mask->dst) {
+			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE,
+					be16_to_cpu(match.key->dst), be16_to_cpu(match.mask->dst),
+					RSWITCH_L4_DST_PORT_OFFSET);
+			if (rc)
+				return rc;
+		}
+	}
+
+	f->param.pf_cascade_index = rswitch_setup_pf(&pf_param);
+	if (f->param.pf_cascade_index < 0)
+		return -E2BIG;
+
+	return 0;
+}
+
 static int rswitch_tc_flower_setup_action(struct rswitch_tc_filter *f,
 				struct flow_rule *rule)
 {
@@ -152,9 +402,7 @@ static int rswitch_tc_flower_replace(struct net_device *dev,
 	struct rswitch_device *rdev = netdev_priv(dev);
 	struct rswitch_private *priv = rdev->priv;
 	struct rswitch_tc_filter *f;
-	struct rswitch_pf_param pf_param = {0};
-	int rc = 0, i;
-	u16 addr_type = 0;
+	int rc;
 
 	if (rswitch_tc_flower_validate_match(rule) ||
 		rswitch_tc_flower_validate_action(rdev, rule)) {
@@ -173,252 +421,14 @@ static int rswitch_tc_flower_replace(struct net_device *dev,
 	f->param.src_ip = 0;
 	f->cookie = cls_flower->cookie;
 
-	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_CONTROL)) {
-		struct flow_match_control match;
-
-		flow_rule_match_control(rule, &match);
-		addr_type = match.key->addr_type;
-	} else if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_IPV4_ADDRS)) {
-		addr_type = FLOW_DISSECTOR_KEY_IPV4_ADDRS;
-	} else if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_IPV6_ADDRS)) {
-		addr_type = FLOW_DISSECTOR_KEY_IPV6_ADDRS;
-	}
-
-	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_BASIC)) {
-		struct flow_match_basic match;
-
-		flow_rule_match_basic(rule, &match);
-
-		if (match.mask->n_proto) {
-			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE,
-					ntohs(match.key->n_proto), ntohs(match.mask->n_proto),
-					RSWITCH_IP_VERSION_OFFSET);
-			if (rc)
-				goto free;
-		}
-
-		if (match.mask->ip_proto) {
-			/* Using one byte in two-byte filter, make offset correction */
-			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE,
-					match.key->ip_proto, match.mask->ip_proto,
-					RSWITCH_IPV4_PROTO_OFFSET - 1);
-			if (rc)
-				goto free;
-		}
-	}
-
-	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_ETH_ADDRS)) {
-		struct flow_match_eth_addrs match;
-
-		flow_rule_match_eth_addrs(rule, &match);
-
-		/*
-		 * There two possible scenarios for both src and dst MAC
-		 * matching (when mask is non-zero):
-		 * - MAC is not masked (mask is ff:...:ff) and we can use
-		 *   1 three-byte filter in expand mode
-		 * - MAC is masked and we need to use 2 three-byte filters.
-		 *   Both MAC and mask will be divided into 2 parts and will
-		 *   be placed to separate filters.
-		 */
-		if (!is_zero_ether_addr(match.mask->src)) {
-			if (is_broadcast_ether_addr(match.mask->src)) {
-				rc = rswitch_init_expand_pf_entry(&pf_param, PF_THREE_BYTE,
-						rswitch_mac_left_half(match.key->src),
-						rswitch_mac_right_half(match.key->src),
-						RSWITCH_MAC_SRC_OFFSET);
-				if (rc)
-					goto free;
-			} else {
-				rc = rswitch_init_mask_pf_entry(&pf_param, PF_THREE_BYTE,
-						rswitch_mac_left_half(match.key->src),
-						rswitch_mac_left_half(match.mask->src),
-						RSWITCH_MAC_SRC_OFFSET);
-				if (rc)
-					goto free;
-
-				rc = rswitch_init_mask_pf_entry(&pf_param, PF_THREE_BYTE,
-						rswitch_mac_right_half(match.key->src),
-						rswitch_mac_right_half(match.mask->src),
-						RSWITCH_MAC_SRC_OFFSET + 3);
-				if (rc)
-					goto free;
-			}
-		}
-
-		if (!is_zero_ether_addr(match.mask->dst)) {
-			if (is_broadcast_ether_addr(match.mask->dst)) {
-				rc = rswitch_init_expand_pf_entry(&pf_param, PF_THREE_BYTE,
-							rswitch_mac_left_half(match.key->dst),
-							rswitch_mac_right_half(match.key->dst),
-							RSWITCH_MAC_DST_OFFSET);
-				if (rc)
-					goto free;
-			} else {
-				rc = rswitch_init_mask_pf_entry(&pf_param, PF_THREE_BYTE,
-						rswitch_mac_left_half(match.key->dst),
-						rswitch_mac_left_half(match.mask->dst),
-						RSWITCH_MAC_DST_OFFSET);
-				if (rc)
-					goto free;;
-
-				rc = rswitch_init_mask_pf_entry(&pf_param, PF_THREE_BYTE,
-						rswitch_mac_right_half(match.key->dst),
-						rswitch_mac_right_half(match.mask->dst),
-						RSWITCH_MAC_DST_OFFSET + 3);
-				if (rc)
-					goto free;
-			}
-		}
-	}
-
-	if (addr_type == FLOW_DISSECTOR_KEY_IPV4_ADDRS) {
-		struct flow_match_ipv4_addrs match;
-
-		flow_rule_match_ipv4_addrs(rule, &match);
-
-		if (match.mask->src) {
-			rc = rswitch_init_mask_pf_entry(&pf_param, PF_FOUR_BYTE,
-					be32_to_cpu(match.key->src), be32_to_cpu(match.mask->src),
-					RSWITCH_IPV4_SRC_OFFSET);
-			if (rc)
-				goto free;
-		}
-
-		if (match.mask->dst) {
-			rc = rswitch_init_mask_pf_entry(&pf_param, PF_FOUR_BYTE,
-					be32_to_cpu(match.key->dst), be32_to_cpu(match.mask->dst),
-					RSWITCH_IPV4_DST_OFFSET);
-			if (rc)
-				goto free;
-		}
-	}
-
-	if (addr_type == FLOW_DISSECTOR_KEY_IPV6_ADDRS) {
-		struct flow_match_ipv6_addrs match;
-
-		flow_rule_match_ipv6_addrs(rule, &match);
-
-		/*
-		 * Approach is the same as for MAC addresses:
-		 * - 2 four-byte filters in expand mode for exact IPv6
-		 * - 4 four-byte filter in mask mode for masked IPv6 (or less
-		 *   if mask has more than 32 zeros at the end)
-		 */
-		if (!rswitch_ipv6_all_zero(&match.mask->src)) {
-			if (rswitch_ipv6_all_set(&match.mask->src)) {
-				rc = rswitch_init_expand_pf_entry(&pf_param, PF_FOUR_BYTE,
-						be32_to_cpu(match.key->src.s6_addr32[0]),
-						be32_to_cpu(match.key->src.s6_addr32[1]),
-						RSWITCH_IPV6_SRC_OFFSET);
-				if (rc)
-					goto free;
-
-				rc = rswitch_init_expand_pf_entry(&pf_param, PF_FOUR_BYTE,
-						be32_to_cpu(match.key->src.s6_addr32[2]),
-						be32_to_cpu(match.key->src.s6_addr32[3]),
-						RSWITCH_IPV6_SRC_OFFSET + 8);
-				if (rc)
-					goto free;
-			} else {
-				/* Walk through all 128-bit or until we reach zero mask in addr */
-				for (i = 0; (i < 4) && match.mask->src.s6_addr32[i]; i++) {
-					rc = rswitch_init_mask_pf_entry(&pf_param, PF_FOUR_BYTE,
-							be32_to_cpu(match.key->src.s6_addr32[i]),
-							be32_to_cpu(match.mask->src.s6_addr32[i]),
-							RSWITCH_IPV6_SRC_OFFSET + 4 * i);
-					if (rc)
-						goto free;
-				}
-			}
-		}
-
-		if (!rswitch_ipv6_all_zero((&match.mask->dst))) {
-			if (rswitch_ipv6_all_set(&match.mask->dst)) {
-				rc = rswitch_init_expand_pf_entry(&pf_param, PF_FOUR_BYTE,
-						be32_to_cpu(match.key->dst.s6_addr32[0]),
-						be32_to_cpu(match.key->dst.s6_addr32[1]),
-						RSWITCH_IPV6_DST_OFFSET);
-				if (rc)
-					goto free;
-
-				rc = rswitch_init_expand_pf_entry(&pf_param, PF_FOUR_BYTE,
-						be32_to_cpu(match.key->dst.s6_addr32[2]),
-						be32_to_cpu(match.key->dst.s6_addr32[3]),
-						RSWITCH_IPV6_DST_OFFSET + 8);
-				if (rc)
-					goto free;
-			} else {
-				/* Walk through all 128-bit or until we reach zero mask in addr */
-				for (i = 0; (i < 4) && match.mask->dst.s6_addr32[i]; i++) {
-					rc = rswitch_init_mask_pf_entry(&pf_param, PF_FOUR_BYTE,
-							be32_to_cpu(match.key->dst.s6_addr32[i]),
-							be32_to_cpu(match.mask->dst.s6_addr32[i]),
-							RSWITCH_IPV6_DST_OFFSET + 4 * i);
-					if (rc)
-						goto free;
-				}
-			}
-		}
-	}
-
-	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_IP)) {
-		struct flow_match_ip match;
-
-		flow_rule_match_ip(rule, &match);
-
-		if (match.mask->tos) {
-			/* Using one byte in two-byte filter, make offset correction */
-			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE, match.key->tos,
-					match.mask->tos, RSWITCH_IPV4_TOS_OFFSET - 1);
-			if (rc)
-				goto free;
-		}
-
-		if (match.mask->ttl) {
-			/* Using one byte in two-byte filter, make offset correction */
-			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE, match.key->ttl,
-					match.mask->ttl, RSWITCH_IPV4_TTL_OFFSET - 1);
-			if (rc)
-				goto free;
-		}
-	}
-
-	if (flow_rule_match_key(rule, FLOW_DISSECTOR_KEY_PORTS)) {
-		struct flow_match_ports match;
-
-		flow_rule_match_ports(rule, &match);
-
-		if (match.mask->src) {
-			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE,
-					be16_to_cpu(match.key->src), be16_to_cpu(match.mask->src),
-					RSWITCH_L4_SRC_PORT_OFFSET);
-			if (rc)
-				goto free;
-		}
-
-		if (match.mask->dst) {
-			rc = rswitch_init_mask_pf_entry(&pf_param, PF_TWO_BYTE,
-					be16_to_cpu(match.key->dst), be16_to_cpu(match.mask->dst),
-					RSWITCH_L4_DST_PORT_OFFSET);
-			if (rc)
-				goto free;
-		}
-	}
+	rc = rswitch_tc_flower_setup_match(f, rule);
+	if (rc)
+		goto free;
 
 	rc = rswitch_tc_flower_setup_action(f, rule);
-	if (rc) {
-		goto free;
-	}
+	if (rc)
+		goto put_pf;
 
-	pf_param.rdev = rdev;
-	pf_param.all_sources = false;
-
-	f->param.pf_cascade_index = rswitch_setup_pf(&pf_param);
-	if (f->param.pf_cascade_index < 0) {
-		rc = -E2BIG;
-		goto free;
-	}
 
 	if (rswitch_add_l3fwd(&f->param)) {
 		rc = -EBUSY;
