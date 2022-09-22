cab2dae6e0933bf2b41729b8a6c2196a3e12cfa0
Dmytro Firsov <dmytro_firsov@epam.com>
2022-09-22 18:19:59 +0300

    drivers: net: renesas: implement VLAN mangle offload for flower and mall filters
    This commit adds implementation for hardware offload of traffic control
    VLAN modify actions for R-Switch flower and matchall filters. It uses
    L23 C-Tag update feature and now supports only 802.1Q VLANs.
    
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- drivers/net/ethernet/renesas/rswitch_tc_matchall.c
+++ drivers/net/ethernet/renesas/rswitch_tc_matchall.c
@@ -23,13 +23,25 @@ static void rswitch_init_mall_redirect_action(struct rswitch_tc_filter *f,
 	cfg->action = f->action;
 	cfg->param.slv = BIT(f->rdev->port);
 	cfg->param.dv = BIT(f->target_rdev->port);
-	if (f->action & ACTION_CHANGE_DMAC) {
+
+	if (f->action & (ACTION_CHANGE_DMAC | ACTION_VLAN_CHANGE)) {
 		cfg->param.l23_info.priv = f->rdev->priv;
-		ether_addr_copy(cfg->param.l23_info.dst_mac, f->dmac);
-		ether_addr_copy(cfg->dmac, f->dmac);
-		cfg->param.l23_info.update_dst_mac = true;
 		cfg->param.l23_info.routing_number = rswitch_rn_get(f->rdev->priv);
-		cfg->param.l23_info.routing_port_valid = BIT(f->rdev->port) | BIT(f->target_rdev->port);
+		cfg->param.l23_info.routing_port_valid = BIT(f->rdev->port) |
+							 BIT(f->target_rdev->port);
+
+		if (f->action & ACTION_CHANGE_DMAC) {
+			ether_addr_copy(cfg->param.l23_info.dst_mac, f->dmac);
+			ether_addr_copy(cfg->dmac, f->dmac);
+			cfg->param.l23_info.update_dst_mac = true;
+		}
+
+		if (f->action & ACTION_VLAN_CHANGE) {
+			cfg->param.l23_info.update_ctag_vlan_id = true;
+			cfg->param.l23_info.update_ctag_vlan_prio = true;
+			cfg->param.l23_info.vlan_id = f->vlan_id;
+			cfg->param.l23_info.vlan_prio = f->vlan_prio;
+		}
 	}
 }
 
@@ -128,11 +140,26 @@ static int rswitch_tc_matchall_replace(struct net_device *ndev,
 		/* Drop in hardware */
 		if (entry->id == FLOW_ACTION_DROP)
 			filter.action |= ACTION_DROP;
+
+		if (entry->id == FLOW_ACTION_VLAN_MANGLE) {
+			if (entry->vlan.proto != ETH_P_8021Q) {
+				pr_err("Unsupported VLAN proto for offload!\n");
+				return -EOPNOTSUPP;
+			}
+
+			filter.vlan_id = entry->vlan.vid;
+			filter.vlan_prio = entry->vlan.prio;
+			filter.action |= ACTION_VLAN_CHANGE;
+		}
 	}
 
 	/* skbmod cannot be offloaded without redirect */
-	if ((filter.action & (ACTION_CHANGE_DMAC | ACTION_MIRRED_REDIRECT)) == ACTION_CHANGE_DMAC)
-		return -EOPNOTSUPP;
+	if (filter.action & (ACTION_CHANGE_DMAC | ACTION_VLAN_CHANGE)) {
+		if (!(filter.action & ACTION_MIRRED_REDIRECT)) {
+			pr_err("DMAC and VLAN change is supported only with redirect\n");
+			return -EOPNOTSUPP;
+		}
+	}
 
 	return rswitch_add_mall_action(&filter, cls_matchall);
 }
