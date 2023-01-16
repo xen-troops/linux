5aab783b26db5a8f87b313c71ce3c8b11ba06671
Ihor Usyk <ihor_usyk@epam.com>
2023-01-16 21:47:45 +0100

    taurus: move common taurus .h files in common folder
    The drivers misc/cldatadrv and gpu/drm/rcar-rvgc use the same files:
    r_taurus_rvgc_protocol.h
    r_taurus_bridge.h
    
    It is reasonable move them into include/linux to share between the drivers.
    
    Signed-off-by: Ihor Usyk <ihor_usyk@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- include/linux/r_taurus_protocol_ids.h
+++ include/linux/r_taurus_protocol_ids.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _R_TAURUS_PROTOCOL_IDS_H_
 #define _R_TAURUS_PROTOCOL_IDS_H_
 
@@ -5,5 +6,6 @@
 #define TAURUS_PROTOCOL_RVGC_ID         (0x01)
 #define TAURUS_PROTOCOL_CAN_ID          (0x02)
 #define TAURUS_PROTOCOL_IPMMUWA_ID      (0x03)
+#define TAURUS_PROTOCOL_CLUSTER_ID      (0x04)
 
 #endif
