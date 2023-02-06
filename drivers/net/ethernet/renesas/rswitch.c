f3a5733389474dea9536aad5fdcbb21c4e7fdb34
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2023-02-06 13:29:31 +0200

    net: renesas: rswitch: add netif_napi_del in VLAN unregister
    After unregistering VLAN, napi should be properly deleted not only
    disabled.
    
    Reviewed-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Signed-off-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>

--- drivers/net/ethernet/renesas/rswitch.c
+++ drivers/net/ethernet/renesas/rswitch.c
@@ -4592,6 +4592,7 @@ static void vlan_dev_unregister(struct net_device *dev)
 	rswitch_rxdmac_free(dev, priv);
 	rswitch_txdmac_free(dev, priv);
 	napi_disable(&rdev->napi);
+	netif_napi_del(&rdev->napi);
 
 	cleanup_all_routes(rdev);
 
