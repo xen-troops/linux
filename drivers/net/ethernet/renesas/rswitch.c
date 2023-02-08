8aa20f7510073cc718c28fc8058b97134012d0fa
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2023-02-08 14:00:18 +0200

    net: renesas: rswitch: fix memory leak with rdev in vlan unregister
    R-Switch device for VLANs are allocated via kzalloc in vlan register
    netdev event, so free memory in VLAN unregister to avoid memory
    leak.
    
    Reviewed-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Signed-off-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>

--- drivers/net/ethernet/renesas/rswitch.c
+++ drivers/net/ethernet/renesas/rswitch.c
@@ -4598,6 +4598,7 @@ static void vlan_dev_unregister(struct net_device *dev)
 
 	list_del(&rdev->list);
 	dev->dev.parent = rdev->vlan_parent;
+	kfree(rdev);
 }
 
 static int vlan_device_event(struct notifier_block *unused, unsigned long event,
