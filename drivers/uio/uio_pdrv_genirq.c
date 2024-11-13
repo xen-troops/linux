renesas-rcar/linux-bsp

uio: uio_pdrv_genirq: Add parameter error in case not define property in node
The patch add parameter error to check device which
has not defined power-domains, clocks and resets
property in node.

Signed-off-by: Tam Nguyen <tam.nguyen.xa@renesas.com>

drivers/uio/uio_pdrv_genirq.c
@@ -43,6 +43,7 @@ struct uio_pdrv_genirq_platdata {
 	struct reset_control *rst;
 	int pwr_cnt;
 	int clk_cnt;
+	bool pd;
 };
 
 static int local_pm_runtime_get_sync(struct uio_pdrv_genirq_platdata *priv);
@@ -154,6 +155,11 @@ static int priv_set_pwr(struct uio_info *info, int value)
 	int ret = 0;
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 
+	if (priv->pd) {
+		dev_err(&priv->pdev->dev, "device has not power-domains to set\n");
+		return -EOPNOTSUPP;
+	}
+
 	if (((value == 0) && priv->pwr_cnt > 0) || ((value != 0)
 		    && priv->pwr_cnt == 0)) {
 		if (value == 0)
@@ -175,6 +181,11 @@ static int priv_get_pwr(struct uio_info *info)
 {
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 
+	if (priv->pd) {
+		dev_err(&priv->pdev->dev, "device has not power-domains to get\n");
+		return -EOPNOTSUPP;
+	}
+
 	dev_dbg(&priv->pdev->dev, "Get power state pwr_cnt=%d, clk_cnt=%d\n",
 		priv->pwr_cnt, priv->clk_cnt);
 
@@ -190,6 +201,11 @@ static int priv_set_clk(struct uio_info *info, int value)
 {
 	int ret = 0;
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
+
+	if (!priv->clk) {
+		dev_err(&priv->pdev->dev, "device has not clk to set\n");
+		return -EOPNOTSUPP;
+	}
 
 	if (value == 0)
 		local_clk_disable(priv);
@@ -209,6 +225,11 @@ static int priv_set_clk(struct uio_info *info, int value)
 static int priv_get_clk(struct uio_info *info)
 {
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
+
+	if (!priv->clk) {
+		dev_err(&priv->pdev->dev, "device has not clk to get\n");
+		return -EOPNOTSUPP;
+	}
 
 	dev_dbg(&priv->pdev->dev, "Get clock state - clk_cnt=%d\n",
 		priv->clk_cnt);
@@ -268,6 +289,11 @@ static int priv_set_rst(struct uio_info *info, int value)
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 	int status, ret;
 
+	if (!priv->rst) {
+		dev_err(&priv->pdev->dev, "device has not reset to set\n");
+		return -EOPNOTSUPP;
+	}
+
 	status = reset_control_status(priv->rst);
 
 	switch (value) {
@@ -294,8 +320,10 @@ static int priv_get_rst(struct uio_info *info)
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 	int status;
 
-	if (!priv->rst)
+	if (!priv->rst) {
+		dev_err(&priv->pdev->dev, "device has not reset to get\n");
 		return -EOPNOTSUPP;
+	}
 
 	status = reset_control_status(priv->rst);
 	dev_dbg(&priv->pdev->dev, "Get reset state 0x%x\n", status);
@@ -535,6 +563,14 @@ static int uio_pdrv_genirq_probe(struct platform_device *pdev)
 	 * hardware and enable clocks at open().
 	 */
 	pm_runtime_enable(&pdev->dev);
+
+	if (!of_property_read_bool(node, "power-domains")) {
+		dev_dbg(&pdev->dev, "device has not power-domains property\n");
+		pm_runtime_get_sync(&pdev->dev);
+		pm_runtime_put_sync(&pdev->dev);
+		pm_runtime_disable(&pdev->dev);
+		priv->pd = 1;
+        }
 
 	ret = devm_add_action_or_reset(&pdev->dev, uio_pdrv_genirq_cleanup,
 				       &pdev->dev);

xen-troops/linux

drivers/uio/uio_pdrv_genirq.c
@@ -43,6 +43,7 @@ struct uio_pdrv_genirq_platdata {
 	struct reset_control *rst;
 	int pwr_cnt;
 	int clk_cnt;
+	bool pd;
 };
 
 static int local_pm_runtime_get_sync(struct uio_pdrv_genirq_platdata *priv);
@@ -146,6 +147,12 @@ static int priv_set_pwr(struct uio_info *info, int value)
 	int ret = 0;
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 
+	if (priv->pd) {
+		dev_dbg(&priv->pdev->dev, "device has not power-domains to set\n");
+		ret = -EOPNOTSUPP;
+		return ret;
+	}
+
 	if (((value == 0) && priv->pwr_cnt > 0) || ((value != 0)
 		    && priv->pwr_cnt == 0)) {
 		if (value == 0)
@@ -167,9 +174,14 @@ static int priv_get_pwr(struct uio_info *info)
 {
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 
+	if (priv->pd) {
+		dev_dbg(&priv->pdev->dev, "device has not power-domains to get\n");
+		priv->pwr_cnt = -EOPNOTSUPP;
+		return priv->pwr_cnt;
+	}
+
 	dev_dbg(&priv->pdev->dev, "Get power state pwr_cnt=%d, clk_cnt=%d\n",
 		priv->pwr_cnt, priv->clk_cnt);
-
 	return priv->pwr_cnt;
 }
 
@@ -182,6 +194,12 @@ static int priv_set_clk(struct uio_info *info, int value)
 {
 	int ret = 0;
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
+
+	if (!priv->clk) {
+		dev_dbg(&priv->pdev->dev, "device has not clk to set\n");
+		ret = -EOPNOTSUPP;
+		return ret;
+	}
 
 	if (value == 0)
 		local_clk_disable(priv);
@@ -202,6 +220,12 @@ static int priv_get_clk(struct uio_info *info)
 {
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 
+	if (!priv->clk) {
+		dev_dbg(&priv->pdev->dev, "device has not clk to get\n");
+		priv->clk_cnt = -EOPNOTSUPP;
+		return priv->clk_cnt;
+	}
+
 	dev_dbg(&priv->pdev->dev, "Get clock state - clk_cnt=%d\n",
 		priv->clk_cnt);
 
@@ -215,6 +239,12 @@ static int priv_clk_get_div(struct uio_info *info)
 	struct clk *parent;
 
 	rate = clk_get_rate(priv->clk);
+	if (!priv->clk) {
+		dev_dbg(&priv->pdev->dev, "device has not clk to get div\n");
+		div = -EOPNOTSUPP;
+		return div;
+	}
+
 	if (!rate)
 		return 0;
 
@@ -260,6 +290,12 @@ static int priv_set_rst(struct uio_info *info, int value)
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 	int status, ret;
 
+	if (!priv->rst) {
+		dev_dbg(&priv->pdev->dev, "device has not reset to set\n");
+		ret = -EOPNOTSUPP;
+		return ret;
+	}
+
 	status = reset_control_status(priv->rst);
 
 	switch (value) {
@@ -286,8 +322,11 @@ static int priv_get_rst(struct uio_info *info)
 	struct uio_pdrv_genirq_platdata *priv = info->priv;
 	int status;
 
-	if (!priv->rst)
-		return -EOPNOTSUPP;
+	if (!priv->rst) {
+		dev_dbg(&priv->pdev->dev, "device has not reset to get\n");
+		status = -EOPNOTSUPP;
+		return status;
+	}
 
 	status = reset_control_status(priv->rst);
 	dev_dbg(&priv->pdev->dev, "Get reset state 0x%x\n", status);
@@ -308,6 +347,10 @@ static int uio_pdrv_genirq_ioctl(struct uio_info *info, unsigned int cmd,
 		break;
 	case UIO_PDRV_GET_PWR:
 		value = priv_get_pwr(info);
+		if (value == -EOPNOTSUPP)
+			ret = value;
+		else
+			ret = 0;
 		if (copy_to_user((int __user *)arg, &value, sizeof(value)))
 			return -EFAULT;
 		arg = value;
@@ -319,12 +362,20 @@ static int uio_pdrv_genirq_ioctl(struct uio_info *info, unsigned int cmd,
 		break;
 	case UIO_PDRV_GET_CLK:
 		value = priv_get_clk(info);
+		if (value == -EOPNOTSUPP)
+			ret = value;
+		else
+			ret = 0;
 		if (copy_to_user((int __user *)arg, &value, sizeof(value)))
 			return -EFAULT;
 		arg = value;
 		break;
 	case UIO_PDRV_CLK_GET_DIV:
 		value = priv_clk_get_div(info);
+		if (value == -EOPNOTSUPP)
+			ret = value;
+		else
+			ret = 0;
 		if (copy_to_user((int __user *)arg, &value, sizeof(value)))
 			return -EFAULT;
 		arg = value;
@@ -340,8 +391,13 @@ static int uio_pdrv_genirq_ioctl(struct uio_info *info, unsigned int cmd,
 		break;
 	case UIO_PDRV_GET_RESET:
 		value = priv_get_rst(info);
+		if (value == -EOPNOTSUPP)
+			ret = value;
+		else
+			ret = 0;
 		if (copy_to_user((int __user *)arg, &value, sizeof(value)))
 			return -EFAULT;
+		arg = value;
 		break;
 	}
 
@@ -527,6 +583,14 @@ static int uio_pdrv_genirq_probe(struct platform_device *pdev)
 	 * hardware and enable clocks at open().
 	 */
 	pm_runtime_enable(&pdev->dev);
+
+	if (!of_property_read_bool(node, "power-domains")) {
+		dev_dbg(&pdev->dev, "device has not power-domains property\n");
+		pm_runtime_get_sync(&pdev->dev);
+		pm_runtime_put_sync(&pdev->dev);
+		pm_runtime_disable(&pdev->dev);
+		priv->pd = 1;
+        }
 
 	ret = devm_add_action_or_reset(&pdev->dev, uio_pdrv_genirq_cleanup,
 				       &pdev->dev);