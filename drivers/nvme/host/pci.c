3c3bc760a44c747ce60f029522718cd81dfd3498
Grygorii Strashko <grygorii_strashko@epam.com>
2024-12-18 18:07:44 +0200

    HACK: drivers: nvme: pci: set dma_mask=32
    Set dma_mask=32 to satisfy Xen vpci.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -2373,7 +2373,7 @@ static int nvme_pci_enable(struct nvme_dev *dev)
 
 	pci_set_master(pdev);
 
-	if (dma_set_mask_and_coherent(dev->dev, DMA_BIT_MASK(64)))
+	if (dma_set_mask_and_coherent(dev->dev, DMA_BIT_MASK(32)))
 		goto disable;
 
 	if (readl(dev->bar + NVME_REG_CSTS) == -1) {
