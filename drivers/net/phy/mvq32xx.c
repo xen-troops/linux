5172d54236f292aa17778accc3531c5083e1966b
Thanh Quan <thanh.quan.xn@renesas.com>
2025-06-28 01:02:25 +0700

    net: phy: mvq32xx: Refactor MVQ32XX PHY driver and update initialization flow
    This patch refactors the MVQ32XX PHY driver to improve structure, reliability,
    and compatibility with new initialization and link training flows.
    
    Signed-off-by: Thanh Quan <thanh.quan.xn@renesas.com>
    Signed-off-by: Tam Nguyen <tam.nguyen.xa@renesas.com>

--- drivers/net/phy/mvq32xx.c
+++ drivers/net/phy/mvq32xx.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0+
-/* PHY driver for Qxxxx
+/* PHY driver for MVQ32xx
  *
  * Copyright (C) 2025 Renesas Electronics Corporation
  */
@@ -13,226 +13,208 @@
 #include <linux/sfp.h>
 #include <linux/netdevice.h>
 
-#define Q3XXX_PHY_ID                    0x002b0b20
-
-static bool loopback_enable = false;
-module_param(loopback_enable, bool, 0644);
-MODULE_PARM_DESC(loopback_enable, "Enable internal PCS loopback for testing");
-
-static int qxxxx_soft_reset(struct phy_device *phydev)
+static int mvq32xx_read32(struct phy_device *phydev, u32 regad)
 {
-	int val = phy_read_mmd(phydev, 0x03, 0x0000);
-	if (val < 0)
-		return val;
-	val |= BIT(15);
-	phy_write_mmd(phydev, 0x03, 0x0000, val);
-	return read_poll_timeout(phy_read_mmd, val, !(val & BIT(15)),
-				10, 100000, false, phydev, 0x03, 0x0000);
+	int low, high;
+
+	/* Set 32-bit register address */
+	if (phy_write_mmd(phydev, 0x1e, 0x28, regad & 0xffff) < 0)
+		return -EIO;
+	if (phy_write_mmd(phydev, 0x1e, 0x29, regad >> 16) < 0)
+		return -EIO;
+
+	/* Trigger read */
+	if (phy_write_mmd(phydev, 0x1e, 0x2c, 0) < 0)
+		return -EIO;
+
+	/* Read data */
+	low = phy_read_mmd(phydev, 0x1e, 0x2d);
+	if (low < 0)
+		return low;
+
+	high = phy_read_mmd(phydev, 0x1e, 0x2e);
+	if (high < 0)
+		return high;
+
+	return (high << 16) | (low & 0xffff);
 }
 
-static int qxxxx_set_loopback(struct phy_device *phydev, bool enable)
+static int mvq32xx_write32(struct phy_device *phydev, u32 regad, u32 val)
 {
-	int val = phy_read_mmd(phydev, 0x03, 0x0912);
-	if (val < 0)
-		return val;
+	/* Set 32-bit register address */
+	if (phy_write_mmd(phydev, 0x1e, 0x28, regad & 0xffff) < 0)
+		return -EIO;
+	if (phy_write_mmd(phydev, 0x1e, 0x29, regad >> 16) < 0)
+		return -EIO;
 
-	val &= ~BIT(14);
-	if (enable)
-		val |= BIT(14);
+	/* Set 32-bit data */
+	if (phy_write_mmd(phydev, 0x1e, 0x2A, val & 0xffff) < 0)
+		return -EIO;
+	if (phy_write_mmd(phydev, 0x1e, 0x2b, val >> 16) < 0)
+		return -EIO;
 
-	return phy_write_mmd(phydev, 0x03, 0x0912, val);
-}
-
-static int qxxxx_set_usx_aneg_enable(struct phy_device *phydev, bool enable)
-{
-	int val = phy_read_mmd(phydev, 0x1e, 0x0964);
-	if (val < 0)
-		return val;
-
-	val &= ~BIT(14);
-	if (enable)
-		val |= BIT(14);
-
-	return phy_write_mmd(phydev, 0x1e, 0x0964, val);
-}
-
-static int qxxxx_enable_restart_aneg(struct phy_device *phydev)
-{
-	int val = phy_read_mmd(phydev, 0x07, 0x0000);
-	if (val < 0)
-		return val;
-
-	val |= BIT(9);
-	return phy_write_mmd(phydev, 0x07, 0x0000, val);
-}
-
-static int qxxxx_config_an_adv2(struct phy_device *phydev)
-{
-	int val = 0;
-	val |= BIT(8);
-	val |= BIT(9);
-	val |= BIT(10);
-	val |= BIT(4);
-	return phy_write_mmd(phydev, 0x1e, 0x080C, val);
-}
-
-static int qxxxx_set_master_mode(struct phy_device *phydev, bool is_master)
-{
-	if (phydev->autoneg == AUTONEG_ENABLE)
-		return 0;
-
-	int val = phy_read_mmd(phydev, 0x01, 0x0834);
-	if (val < 0)
-		return val;
-	val = is_master ? (val | BIT(14)) : (val & ~BIT(14));
-	return phy_write_mmd(phydev, 0x01, 0x0834, val);
-}
-
-static int qxxxx_set_speed(struct phy_device *phydev, u8 speed_code)
-{
-	int val = phy_read_mmd(phydev, 0x03, 0x0000);
-	if (val < 0)
-		return val;
-
-	val |= BIT(13);
-	val |= BIT(6);
-	val &= ~GENMASK(5, 2);
-	val |= (speed_code << 2);
-	return phy_write_mmd(phydev, 0x03, 0x0000, val);
-}
-
-static int qxxxx_config_aneg(struct phy_device *phydev)
-{
-	int ret;
-
-	if (!phydev->autoneg)
-		return 0;
-
-	ret = qxxxx_set_usx_aneg_enable(phydev, true);
-	if (ret)
-		return ret;
+	/* Trigger write */
+	if (phy_write_mmd(phydev, 0x1e, 0x2c, 1) < 0)
+		return -EIO;
 
 	return 0;
 }
 
-static int qxxxx_config_init(struct phy_device *phydev)
+static void mvq32xx_update_bits32(struct phy_device *phydev, u32 regad, u32 mask, u32 val)
 {
-	int ret, val;
+	u32 tmp;
 
-	ret = qxxxx_soft_reset(phydev);
-	if (ret)
-		return ret;
+	tmp = mvq32xx_read32(phydev, regad);;
+	tmp = (tmp & ~mask) | (val & mask);
+	mvq32xx_write32(phydev, regad, tmp);
+}
 
-	val = phy_read_mmd(phydev, 0x03, 0x0000);
-	if (val >= 0 && (val & BIT(11))) {
-		val &= ~BIT(11);
-		phy_write_mmd(phydev, 0x03, 0x0000, val);
-		qxxxx_soft_reset(phydev);
-	}
+static int mvq32xx_config_init(struct phy_device *phydev)
+{
+	u32 val;
 
-	val = BIT(15) |
-	      BIT(14) |
-	      BIT(10) |
-	      BIT(9) |
-	      BIT(8) |
-	      (1 << 7) |
-	      (3 << 4);
-	phy_write_mmd(phydev, 0x1E, 0x0964, val);
+	phydev_info(phydev, "%s: MVQ32XX init\n", __func__);
 
-	ret = qxxxx_set_master_mode(phydev, true);
-	if (ret)
-		return ret;
+	/* AN + USXGMII AN + advertise all + master + 10G + USXGMII */
+	val = BIT(15)              /* Line Auto-Negotiation Enable */
+	    | BIT(14)              /* USXGMII Auto-Negotiation Enable */
+	    | BIT(10) | BIT(9) | BIT(8)    /* Advertise 10G/5G/2.5G */
+	    | BIT(7)               /* Prefer Master */
+	    | (3 << 4)             /* Line Rate = 10G */
+	    | (7 << 0);            /* System PCS = 10G USXGMII */
 
-	if (phydev->autoneg == AUTONEG_ENABLE) {
-		ret = qxxxx_config_an_adv2(phydev);
-		if (ret)
-			return ret;
+	mvq32xx_write32(phydev, 0x40780964, val);
 
-		ret = qxxxx_enable_restart_aneg(phydev);
-		if (ret)
-			return ret;
-	} else {
-		u8 speed_code;
+	/* Exit from HCS state */
+	mvq32xx_update_bits32(phydev, 0x407809C8, BIT(1), BIT(1));
 
-		switch (phydev->speed) {
-		case SPEED_10000:
-			speed_code = 0x00;
-			break;
-		case SPEED_5000:
-			speed_code = 0x08;
-			break;
-		case SPEED_2500:
-			speed_code = 0x07;
-			break;
-		default:
-			speed_code = 0x07;
-		}
-
-		ret = qxxxx_set_speed(phydev, speed_code);
-		if (ret)
-			return ret;
-	}
-
-	if (loopback_enable)
-		qxxxx_set_loopback(phydev, true);
-
-	val = phy_read_mmd(phydev, 0x03, 0x0000);
-	if (val >= 0)
-		dev_info(&phydev->mdio.dev, "[QXXXX] PCS_CTRL1 = 0x%04x\n", val);
+	/* Disable System PCS loopback */
+	val = phy_read_mmd(phydev, MDIO_MMD_PCS, 0x0912);
+	val &= ~BIT(14);
+	phy_write_mmd(phydev, MDIO_MMD_PCS, 0x0912, val);
 
 	return 0;
 }
 
-static int qxxxx_read_status(struct phy_device *phydev)
+static int mvq32xx_read_status(struct phy_device *phydev)
 {
 	int val;
+	bool linkStatus = false;
 
-	phydev->link = 0;
-	phydev->speed = SPEED_UNKNOWN;
-	phydev->duplex = DUPLEX_UNKNOWN;
-
-	val = phy_read_mmd(phydev, 0x03, 0x0913);
-	if (val < 0)
-		return val;
-
-	if (val & BIT(2))
-		phydev->link = 1;
-	else
-		return 0;
-
-	val = phy_read_mmd(phydev, 0x03, 0x0000);
-	if (((val >> 13) & 1) && ((val >> 6) & 1)) {
-		switch ((val >> 2) & 0xF) {
-		case 0x0:
-			phydev->speed = SPEED_10000;
-			break;
-		case 0x8:
-			phydev->speed = SPEED_5000;
-			break;
-		case 0x7:
-			phydev->speed = SPEED_2500;
+	/* PMA link check (Line-side) */
+	for (int i = 0; i < 300; i++) {
+		val = phy_read_mmd(phydev, MDIO_MMD_PMAPMD, 0x0906);
+		if ((val & BIT(0)) != 0) {
+			phydev->link = 1;
+			phydev_dbg(phydev, "%s: PMA (Line) Linkup OK\n", __func__);
+			linkStatus = true;
 			break;
 		}
+		usleep_range(1000, 2000);
+	}
+	if (!linkStatus)
+		phydev_dbg(phydev, "%s: PMA (Line) Linkup NG\n", __func__);
+
+	/* Line Device Mode: Master/Slave */
+	val = mvq32xx_read32(phydev, 0x40780964);
+	if (val & BIT(7))
+		phydev_dbg(phydev, "%s: Line Device Mode: Master\n", __func__);
+	else
+		phydev_dbg(phydev, "%s: Line Device Mode: Slave\n", __func__);
+
+	/* Line Rate (forced mode) */
+	switch ((val >> 4) & 0x3) {
+	case 1:
+		phydev->speed = SPEED_2500;
+		phydev_dbg(phydev, "%s: Line Rate = 2.5G\n", __func__);
+		break;
+	case 2:
+		phydev->speed = SPEED_5000;
+		phydev_dbg(phydev, "%s: Line Rate = 5G\n", __func__);
+		break;
+	case 3:
+		phydev->speed = SPEED_10000;
+		phydev_dbg(phydev, "%s: Line Rate = 10G\n", __func__);
+		break;
+	default:
+		phydev->speed = SPEED_UNKNOWN;
+		phydev_dbg(phydev, "%s: Line Rate = Unknown\n", __func__);
+		break;
 	}
 
+	/* PCS link check (System-side) */
+	linkStatus = false;
+	for (int i = 0; i < 300; i++) {
+		val = phy_read_mmd(phydev, MDIO_MMD_PCS, 0x3C7F);
+		if (val & BIT(12)) {
+			phydev_dbg(phydev, "%s: PCS (System) Linkup OK\n", __func__);
+			linkStatus = true;
+			break;
+		}
+		usleep_range(1000, 2000);
+	}
+	if (!linkStatus)
+		phydev_dbg(phydev, "%s: PCS (System) Linkup NG\n", __func__);
+
+	/* System Interface Mode */
+	val = mvq32xx_read32(phydev, 0x40780964);
+	switch (val & 0x7) {
+	case 1:
+		phydev_dbg(phydev, "%s: Sys Mode: 2500 BaseX\n", __func__);
+		break;
+	case 2:
+		phydev_dbg(phydev, "%s: Sys Mode: 2.5G BaseX\n", __func__);
+		break;
+	case 3:
+		phydev_dbg(phydev, "%s: Sys Mode: 5G BaseR\n", __func__);
+		break;
+	case 4:
+		phydev_dbg(phydev, "%s: Sys Mode: 5G BaseR\n", __func__);
+		break;
+	case 5:
+		phydev_dbg(phydev, "%s: Sys Mode: 10G BaseR\n", __func__);
+		break;
+	case 6:
+		phydev_dbg(phydev, "%s: Sys Mode: 5G USXGMII\n", __func__);
+		break;
+	case 7:
+		phydev_dbg(phydev, "%s: Sys Mode: 10G USXGMII\n", __func__);
+		break;
+	default:
+		phydev_dbg(phydev, "%s: Sys Mode: Unknown\n", __func__);
+		break;
+	}
+
+	/* Duplex/Autoneg default assumption */
 	phydev->duplex = DUPLEX_FULL;
+	phydev->autoneg = AUTONEG_ENABLE;
 
 	return 0;
 }
 
-static struct phy_driver QXXXX_driver[] = {
+static int mvq32xx_probe(struct phy_device *phydev)
+{
+	return 0;
+}
+
+static void mvq32xx_remove(struct phy_device *phydev)
+{
+	phydev->priv = NULL;
+}
+
+static struct phy_driver mvq32xx_driver[] = {
 	{
-		PHY_ID_MATCH_EXACT(Q3XXX_PHY_ID),
-		.name		= "PHY QXXXX",
-		.config_init	= qxxxx_config_init,
-		.config_aneg	= qxxxx_config_aneg,
-		.read_status	= qxxxx_read_status,
-		.soft_reset	= qxxxx_soft_reset,
-		.features	= PHY_10GBIT_FEATURES,
+		PHY_ID_MATCH_EXACT(0x002b0b20),
+		.name		= "MVQ32xx",
+		.probe		= mvq32xx_probe,
+		.remove		= mvq32xx_remove,
+		.config_init	= mvq32xx_config_init,
+		.read_status	= mvq32xx_read_status,
+		.features	= PHY_GBIT_FEATURES,
 	},
 };
 
-module_phy_driver(QXXXX_driver);
+module_phy_driver(mvq32xx_driver);
 
-MODULE_DESCRIPTION("Marvell QXXXX PHY driver");
+MODULE_DESCRIPTION("MVQ32xx PHY driver");
 MODULE_LICENSE("GPL");
