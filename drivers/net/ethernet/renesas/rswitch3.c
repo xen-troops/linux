4f3d6cf98c12d2bd0f1634fd246c5bbbdd140c88
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2025-10-31 20:11:40 +0200

    net: ethernet: rsw3: fix unitialized spinlock
    The spinlock should be initialized by spin_lock_init, but currently, it
    is missed, which may cause issues.
    
    Signed-off-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>

--- drivers/net/ethernet/renesas/rswitch3.c
+++ drivers/net/ethernet/renesas/rswitch3.c
@@ -2759,6 +2759,7 @@ static int renesas_eth_sw_probe(struct platform_device *pdev)
 		ret = -EINVAL;
 		goto err_sw_probe;
 	}
+	spin_lock_init(&priv->lock);
 
 	if (!parallel_mode)
 		parallel_mode = of_property_read_bool(pdev->dev.of_node, "parallel_mode");
